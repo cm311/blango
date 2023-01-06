792253e6f250d8b3f8f02bfcccf34adcfacf149b
Colin Moriarity <grahf14@gmail.com>
2023-01-06 10:54:06 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,19 +2,49 @@ from django import template
 from django.utils.html import escape, format_html
 from django.utils.safestring import mark_safe
 from django.contrib.auth import get_user_model
+from blog.models import Post
 
 user_model = get_user_model()
 register = template.Library()
 
-#filters must be registered so the other functions dont get picked up by django
-@register.filter
-def author_details(author, current_user):
-    if not isinstance(author, user_model):
-        # return empty string as safe default
-        return ""
+
+#can query for extra data using inclusion tag.  Here we get some post objects
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
+
+
+
+
+#tags start with %, like url, includes, extends.  filters act on objects by using |
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}"', extra_classes)
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+    request = context["request"]
+    current_user = request.user
+    post = context["post"]
+    author = post.author
 
     if author == current_user:
-      return format_html("<strong>me</strong>")
+        return format_html("<strong>me</strong>")
 
     if author.first_name and author.last_name:
         name = f"{author.first_name} {author.last_name}"
@@ -28,4 +58,5 @@ def author_details(author, current_user):
         prefix = ""
         suffix = ""
 
-    return format_html('{}{}{}', prefix, name, suffix)
+    return format_html("{}{}{}", prefix, name, suffix)
+
