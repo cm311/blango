ab2134f0c8ee98d5967856aa198287c1ae0b1f9e
Colin Moriarity <grahf14@gmail.com>
2023-01-07 13:01:14 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 import logging
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers
 from blog.models import Post
 from blog.forms import CommentForm
 
@@ -9,7 +11,17 @@ from blog.forms import CommentForm
 logger = logging.getLogger(__name__)
 
 # Create your views here.
+
+
+#@cache_page(300)
+
+#params are names of the headers that will cause the response to vary
+#@vary_on_headers("Cookie")
 def index(request):
+  #Post object only queried every 300 seconds, all other responses come
+  #from cache.  Dont cache stuff whose content relies on logged in user.
+  #from django.http import HttpResponse
+  #return HttpResponse(str(request.user).encode("ascii"))
   #only load posts that have a publication date in the past
   posts = Post.objects.filter(published_at__lte=timezone.now())
   logger.debug("Got %d posts", len(posts))
