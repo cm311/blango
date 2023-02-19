e02a9feac31fa25033def31e287ac15c20cdf766
Colin Moriarity <grahf14@gmail.com>
2023-02-19 15:16:29 +0000

    Finish nested relationships

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
-from blog.models import Post, Tag
+from blog.models import Post, Tag, Comment
 from blango_auth.models import User
 
+class TagField(serializers.SlugRelatedField):
+  def to_internal_Value(self, data):
+    try:
+      return self.get_queryset().get_or_create(value=data.lower())[0]
+    except (TypeError, ValueError):
+      self.fail(f"Tag value{data} is invalid")
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ["first_name", "last_name", "email"]
+
+class CommentSerializer(serializers.ModelSerializer):
+  id = serializers.IntegerField(required=False)
+  creator = UserSerializer(read_only=True)
+
+  class Meta:
+    model = Comment
+    fields = ["id", "creator", "content", "modifed_at", "created_at"]
+    readonly = ["modifed_at", "created_at"]
 
 class PostSerializer(serializers.ModelSerializer):
   tags = serializers.SlugRelatedField(
@@ -17,8 +37,24 @@ class PostSerializer(serializers.ModelSerializer):
     fields = "__all__"
     readonly = ["modifed_at", "created_at"]
 
-class UserSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = User
-    fields = ["first_name", "last_name", "email"]
+class PostDetailSerializer(PostSerializer):
+  comments = CommentSerializer(many=True)
+
+  def update(self, instance, validated_data):
+    comments = validated_data.pop("comments")
+
+    instance = super(PostDetailSerializer, self).update(instance, validated_data)
+
+    for comment_data in comments:
+      if comment_data in comments:
+        #comment has an ID so it was pre existing
+        continue
+
+      comment = Coment(**comment_data)
+      comment.creator = self.context["request"].user
+      comment.content_object = instance
+      comment.save()
+
+    return instance
+
 
